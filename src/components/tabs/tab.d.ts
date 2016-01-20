b8dae6a16e0afe3ada36f531228353f44e6c1c1a
Sobieslaw Hrehorowicz <sobieslaw.hrehorowicz@os.uk>
2016-01-20 16:38:14 +0000

    feat(OsTabs, OsTab): components for tab features
    * OsTabs/OsTab directive

---
+++ src/components/tabs/tab.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../../typings/main.d.ts" />
+export declare class OsTab {
+    static $inject: string[];
+    $osTabTransclude: any;
+    constructor($element: ng.IRootElementService, $transclude: ng.ITranscludeFunction);
+}
