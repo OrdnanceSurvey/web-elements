66c2d499babc62252b8ebe24067e3c88e45e3ac7
csnyders <craig.snyders@os.uk>
2016-02-10 13:42:46 +0000

    chore: Fix npm dependencies
    Fix devDependencies to remove complaint about needing peerDependencies installed to run: webpack --config webpack.demos.config.js

--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="main/ambient/angular-material/angular-material.d.ts" />
 /// <reference path="main/ambient/angular/angular.d.ts" />
 /// <reference path="main/ambient/jquery/jquery.d.ts" />
+/// <reference path="main/ambient/openlayers/openlayers.d.ts" />
 /// <reference path="main/ambient/shim/shim.d.ts" />
--- typings/main/ambient/angular/angular.d.ts
+++ typings/main/ambient/angular/angular.d.ts
@@ -1,4 +1,4 @@
-// Compiled using typings@0.6.2
+// Compiled using typings@0.6.5
 // Source: https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/739b784dae633dee826fadaf83e5287c753f3d44/angularjs/angular.d.ts
 // Type definitions for Angular JS 1.4+
 // Project: http://angularjs.org
