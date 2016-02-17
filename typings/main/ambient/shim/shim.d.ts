54a410bc453d254d161414ca5c73e86756809cec
csnyders <craig.snyders@os.uk>
2016-02-17 18:16:17 +0000

    chore: Remove Typings because it should be pulled down, same as npm

--- typings/main/ambient/shim/shim.d.ts
+++
@@ -1,7 +0,0 @@
-// Compiled using typings@0.6.5
-// Source: shim.d.ts
-// shim.d.ts
-// contains shim functions to make the TS compiler happy
-
-// the require function is injected by webpack so we can require CSS/LESS files
-declare function require(path:string): any;
