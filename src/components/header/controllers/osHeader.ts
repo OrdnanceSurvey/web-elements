30646a1745332f4a5feafdc3e6d811db8fdbe521
Sobieslaw Hrehorowicz <sobieslaw.hrehorowicz@xstream.net>
2016-02-18 11:33:26 +0100

    feat(osSearch, osHeader): adjust search column width and search visibility

--- src/components/header/controllers/osHeader.ts
+++ src/components/header/controllers/osHeader.ts
@@ -1,11 +1,16 @@
 /// <reference path="../../../../typings/main.d.ts" />
 
 export class OsHeader {
-  static $inject = ['$element'];
+  static $inject = ['$element', '$scope'];
 
   title: string;
+  useSearch: boolean;
 
-  constructor(private $element: ng.IRootElementService) {}
+  constructor(private $element: ng.IRootElementService, private $scope: ng.IScope) {}
+
+  openSearch() : void {
+    this.$scope.$broadcast('osHeader.openSearch');
+  }
 
 }
 
