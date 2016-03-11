152e085db332905db52622c9cddcb7b1da10af27
Sobieslaw Hrehorowicz <sobieslaw.hrehorowicz@xstream.net>
2016-03-11 16:43:00 +0100

    unit test: do not merge

--- test/sample_test.js
+++ test/sample_test.js
@@ -1,13 +1,13 @@
 describe("sample test", function() {
 
-  beforeEach(angular.mock.module('osElements'));
-
-  var $controller;
-
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
-    console.log('$controller', $controller);
-  }));
+  //beforeEach(angular.mock.module('osElements'));
+  //
+  //var $controller;
+  //
+  //beforeEach(inject(function(_$controller_){
+  //  $controller = _$controller_;
+  //  console.log('$controller', $controller);
+  //}));
 
   it("should run another test", function() {
     expect(true).toBe(true);
