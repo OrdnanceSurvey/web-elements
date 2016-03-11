ebfa83d5f1383899c0970fbd477f5f03bd93051e
csnyders <craig.snyders@os.uk>
2016-03-11 14:44:00 +0000

    fix(osSelect): Don't show clear button if no function is provided

---
+++ test/sample_test.js
@@ -0,0 +1,16 @@
+describe("sample test", function() {
+
+  //beforeEach(angular.mock.module('osElements'));
+  //
+  //var $controller;
+  //
+  //beforeEach(inject(function(_$controller_){
+  //  $controller = _$controller_;
+  //  console.log('$controller', $controller);
+  //}));
+
+  it("should run another test", function() {
+    expect(true).toBe(true);
+  });
+
+});
