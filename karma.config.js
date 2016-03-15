4a633051f1626447a8c986c9050dbf8722dea7fb
Sobieslaw Hrehorowicz <sobieslaw.hrehorowicz@xstream.net>
2016-03-15 12:11:12 +0100

    chore(build): karma, unit test config, webpack typescript loader and linter
    * karma config and integration
    * npm test command
    * unit test config
    * webpack typescript loader
    * webpack typescript linter

--- karma.config.js
+++ karma.config.js
@@ -4,25 +4,28 @@ var webpackConfig = require("./webpack.config.js");
 
 webpackConfig.devtool = 'inline-source-map';
 
-console.log('webpackConfig', webpackConfig);
-
-
 module.exports = function(config) {
   config.set({
     basePath: '.',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'jasmine-matchers'],
     files: [
       './node_modules/angular/angular.js',
       './node_modules/angular-mocks/angular-mocks.js',
-      // tests
-      'test/*_test.js',
-      'test/**/*_test.js'
+      './node_modules/angular-animate/angular-animate.js',
+      './node_modules/angular-aria/angular-aria.js',
+      './node_modules/angular-component/dist/angular-component.js', // polyfill component API for angular < 1.5
+      './node_modules/angular-messages/angular-messages.js',
+      './node_modules/angular-material/angular-material.js',
+      './node_modules/angular-sanitize/angular-sanitize.js',
+      './node_modules/rx/dist/rx.js',
+      './node_modules/rx-angular/dist/rx.angular.js',
+
+      // unit test main entry
+      'test/unit/test_index.js'
     ],
 
     preprocessors: {
-      // add webpack as preprocessor
-      'test/*_test.js': ['webpack'],
-      'test/**/*_test.js': ['webpack']
+      'test/unit/test_index.js': ['webpack']
     },
 
     logLevel: config.LOG_DEBUG,
@@ -30,20 +33,20 @@ module.exports = function(config) {
     webpack: webpackConfig,
 
     webpackMiddleware: {
-      // webpack-dev-middleware configuration
-      // i. e.
       stats: {
         colors: true
       },
-      noInfo: false
+      noInfo: true
     },
+
     //browsers: ['PhantomJS'],
     browsers: ['Chrome'],
     plugins: [
       "karma-webpack",
       "karma-jasmine",
       "karma-chrome-launcher",
-      "karma-phantomjs-launcher"
+      "karma-phantomjs-launcher",
+      "karma-jasmine-matchers"
     ]
   });
 };
