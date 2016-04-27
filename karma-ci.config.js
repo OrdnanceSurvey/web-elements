14d68ae795b64e17132f834e2a89b7b99d55e82f
csnyders <craig.snyders@os.uk>
2016-04-27 16:41:02 +0100

    ci: Correct reporters for CI (SauceLabs)

--- karma-ci.config.js
+++ karma-ci.config.js
@@ -73,7 +73,7 @@ module.exports = function(config) {
       noInfo: true
     },
 
-    reporters: ['dots', 'saucelabs'],
+    reporters: ['dots', 'saucelabs', 'nested', 'coverage', 'threshold', 'coveralls'],
 
     sauceLabs: {
       testName: 'OS Web elements'
