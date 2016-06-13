8759b134d623d5b2da3bc6fe0816a885554770fa
csnyders <craig.snyders@os.uk>
2016-06-13 10:11:59 +0100

    fix(zoombar): support ngChange function

--- test/unit/components/zoombar/zoombar_test.js
+++ test/unit/components/zoombar/zoombar_test.js
@@ -1,12 +1,14 @@
 describe("osZoombar | ", function() {
-  var controller, elementMock;
+  var controller, elementMock, changeFn;
 
   beforeEach(angular.mock.module('osElements'));
 
   beforeEach(inject(function($controller){
     elementMock = { elementMock: true};
 
-    controller = $controller('OsZoombarController', { $element: elementMock}, {ngModel: 4, zoomMin: 0, zoomMax: 7});
+    changeFn = jasmine.createSpy('changeFn');
+
+    controller = $controller('OsZoombarController', { $element: elementMock}, {ngModel: 4, zoomMin: 0, zoomMax: 7, ngChange: changeFn});
   }));
 
   it("should execute constructor", function() {
@@ -24,36 +26,46 @@ describe("osZoombar | ", function() {
     expect(controller.zoomMax).toEqual(7);
   });
 
-  it("should zoom out", function() {
+  it("should zoom out and fire change function", function() {
     expect(controller.ngModel).toEqual(4);
+    expect(changeFn).not.toHaveBeenCalled();
     controller.zoomOut();
     expect(controller.ngModel).toEqual(3);
+    expect(changeFn).toHaveBeenCalledWith(3); // changefn should be called with new zoom level
+    expect(changeFn.calls.count()).toEqual(1); // only called once
   });
 
   it("should limit zoom out level", function() {
     expect(controller.ngModel).toEqual(4);
+    expect(changeFn).not.toHaveBeenCalled();
     controller.zoomOut();
     controller.zoomOut();
     controller.zoomOut();
     controller.zoomOut();
     controller.zoomOut();
     expect(controller.ngModel).toEqual(0);
+    expect(changeFn.calls.count()).toEqual(4); // should only have been called 4 times once min of 0 is reached
   });
 
-  it("should zoom in", function() {
+  it("should zoom in and fire change function", function() {
     expect(controller.ngModel).toEqual(4);
+    expect(changeFn).not.toHaveBeenCalled();
     controller.zoomIn();
     expect(controller.ngModel).toEqual(5);
+    expect(changeFn).toHaveBeenCalledWith(5); // changefn should be called with new zoom level
+    expect(changeFn.calls.count()).toEqual(1); // only called once
   });
 
-  it("should limit zoom out level", function() {
+  it("should limit zoom in level", function() {
     expect(controller.ngModel).toEqual(4);
+    expect(changeFn).not.toHaveBeenCalled();
     controller.zoomIn();
     controller.zoomIn();
     controller.zoomIn();
     controller.zoomIn();
     controller.zoomIn();
     expect(controller.ngModel).toEqual(7);
+    expect(changeFn.calls.count()).toEqual(3); // should only have been called 3 times once max of 7 is reached
   });
 
 });
